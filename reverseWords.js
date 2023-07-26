e2a67b5e2a361a481144709997d644d821da383a
Christian Johnson <charcoaldust@mac.com>
2023-07-25 17:17:15 -0700

    Added Big O notation

--- reverseWords.js
+++ reverseWords.js
@@ -1,14 +1,19 @@
-// * reverse the order of words in a string.  Can not use .reverse method.
+// ** reverse the letters of each word in a string.  Can not use .reverse method.
+// Time Complexity: O(n * m), where n is the number of words, and m is the maximum length of a word within the originalArray
+// Space complexity:O(n + k)where n is the number of words and k is the length of the longest word
 
 const reverseWords = (str) => {
   const originalArray = str.split(" ");
   const newArray = [];
-  for (let i = originalArray.length - 1; i >= 0; i--) {
-    newArray.push(originalArray[i]);
-  }
+  originalArray.forEach((word) => {
+    let reverseWord = "";
+    for (let i = word.length - 1; i >= 0; i--) {
+      reverseWord += word[i];
+    }
+    console.log(reverseWord);
+    newArray.push(reverseWord);
+  });
   return newArray.join(" ");
 };
 
-console.log(reverseWords("Grover slept on the couch last night."));
-
-//Time Complexity is O(n) with n being the length of the original string
+console.log(reverseWords("Grover slept on the couch last night.")); //revorG tpels no eht hcuoc tsal .thgin
